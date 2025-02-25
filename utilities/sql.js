d87ff7ae21cdf46d0a21c7aa85be6866877ced24
Louis Loewen <louisloewen@Louiss-MacBook-Pro-2.local>
2025-02-25 10:57:38 -0600

    Avance

---
+++ utilities/sql.js
@@ -0,0 +1,24 @@
+import sql from "msql";
+
+const sqlConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    server: process.env.DB_SERVER,
+    pool: {
+      max: 10,
+      min: 0,
+      idleTimeoutMillis: 30000
+    },
+    options: {
+      encrypt: true, // for azure
+      trustServerCertificate: false // change to true for local dev / self-signed certs
+    }
+  }
+  
+
+  const sqlConnect = async () => {
+    return await sql.connect(sqlConfig);
+  };
+
+  export {sqlConnect};
