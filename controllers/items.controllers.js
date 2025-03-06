e5ff6fa689a3bf8dbb84e081010c148c5eeecb6b
Louis Loewen <louisloewen@Louiss-MacBook-Pro-2.local>
2025-03-06 12:26:58 -0600

    Actividad Terminada

--- controllers/items.controllers.js
+++ controllers/items.controllers.js
@@ -3,5 +3,45 @@ import { sqlConnect, sql } from "../utilities/sql.js";
 export const getItems = async(req, res) => {
     const pool = await sqlConnect();
     const data = await pool.request().query("select * from items");
-    console.log(data);
+    // console.log(data.recordset);
+    res.json(data.recordset);
+};
+
+export const getItem = async(req, res) => {
+    const pool = await sqlConnect();
+    const data = await pool.request().input("myId", sql.Int, req.params.id).query("select * from items where id = @myId");
+    // console.log(data.recordset);
+    res.json(data.recordset);
+};
+
+export const postItem = async(req, res) => {
+    const pool = await sqlConnect();
+    const data = await pool.request()
+    .input("name", sql.VarChar, req.body.name)
+    .input("price", sql.Float, req.body.price)
+    .query("insert into items(name, price) values (@name, @price)");
+    // console.log(data.recordset);
+    res.status(200).json({ operation: true });
+};
+
+export const putItem = async(req, res) => {
+    const pool = await sqlConnect();
+    const data = await pool
+        .request()
+        .input("Id", sql.Int, req.params.id)
+        .input("name", sql.VarChar, req.body.name)
+        .input("price", sql.Float, req.body.price)
+        .query("update items set (name=@name, price=@price) where id=@id");
+    // console.log(data.recordset);
+    res.status(200).json({ operation: true })
+};
+
+export const deleteItem = async(req, res) => {
+    const pool = await sqlConnect();
+    const data = await pool
+        .request()
+        .input("Id", sql.Int, req.params.id)
+        .query("delete from items where id=@id");
+    // console.log(data.recordset);
+    res.status(200).json({ operation: true })
 };
--- index.js
+++ index.js
@@ -4,10 +4,13 @@ dotenv.config();
 import express from "express";
 import indexRoutes from "./routes/index.routes.js"
 import itemsRoutes from "./routes/items.route.js"
+import loginRoutes from "./routes/login.routes.js"
 
 const app = express();
 
+app.use(express.json());
 app.use(indexRoutes);
 app.use(itemsRoutes);
+app.use(loginRoutes);
 
 app.listen(3000, console.log("http://localhost:3000"));
